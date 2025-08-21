316df93e8ca6bba563f3438a744fc62f3e8cca04
Katy <89160388957@mail.ru>
2025-08-21 16:41:07 +0300

    №6 Ф6 добавить HistoryList, удаление дубликатов. Тесты на них + выровнены строки

--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -3,7 +3,7 @@ package task;
 public class Subtask extends Task {
     private int epicId;
 
-    public Subtask (String title, String description, int epicId) {
+    public Subtask(String title, String description, int epicId) {
         super(title, description);
         this.epicId = epicId;
     }
